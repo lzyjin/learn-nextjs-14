42241580ce7913b3472ca12f6ee9436b2634f3d0
lzyjin <1247y.j@gmail.com>
2024-11-14 14:14:50 +0900

    fix type error of dynamic metadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -6,12 +6,6 @@ interface IParams {
   params: Promise<{ id: string }>
 }
 
-interface IProps {
-  params: {
-    id: string
-  }
-}
-
 export async function generateMetadata({params}: IParams) {
   const id = (await params).id;
   const movie = await getMovie(id); // 처음으로 데이터 fetching함 -> MovieInfo에서는 api 요청하지 않고 캐싱됌
@@ -21,7 +15,9 @@ export async function generateMetadata({params}: IParams) {
   }
 }
 
-export default async function MovieDetail({params: {id}}: IProps) {
+export default async function MovieDetail({params}: IParams) {
+  const id = (await params).id;
+
   return (
     <div>
       <Suspense fallback={ <h1>loading movie info</h1> }>
